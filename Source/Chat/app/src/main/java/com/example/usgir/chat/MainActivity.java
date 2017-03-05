4558cfc3e1c6022231fc5448d7561f37ba6a4ee2
Girish85 <usgirishnagella@gmail.com>
2017-03-04 22:35:01 -0800

    Adding the Chat Application for students. Check it out and let's integrate it to main application.

---
+++ Source/Chat/app/src/main/java/com/example/usgir/chat/MainActivity.java
@@ -0,0 +1,52 @@
+package com.example.usgir.chat;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        final TextView textView = (TextView)findViewById(R.id.textView);
+        FirebaseDatabase database=FirebaseDatabase.getInstance();
+        DatabaseReference reference=database.getReference("MSG");
+        reference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                String value = dataSnapshot.getValue().toString();
+                textView.setText(value);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                Toast.makeText(MainActivity.this,"Sorry limit reached",Toast.LENGTH_LONG).show();
+            }
+        });
+        textView.setVisibility(View.VISIBLE);
+    }
+    public void onclick(View view)
+    {
+        FirebaseDatabase database=FirebaseDatabase.getInstance();
+        DatabaseReference reference=database.getReference("MSG");
+        EditText e = (EditText)findViewById(R.id.editText2);
+        String x = e.getText().toString();
+        EditText editText = (EditText)findViewById(R.id.editText);
+        String s = editText.getText().toString();
+        reference.setValue(x+":"+s);
+        editText.setText("");
+        e.setVisibility(View.GONE);
+    }
+}
